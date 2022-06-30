bb839f451bd7cce81ab78cdecbba4020781440b7
Peter Goodman <peter.goodman@gmail.com>
2022-06-30 15:56:56 -0400

    Make it so that the mx-print-call-graph tool can only show paths between two functions

--- bin/Examples/PrintCallGraph.cpp
+++ bin/Examples/PrintCallGraph.cpp
@@ -7,14 +7,18 @@
 #include <gflags/gflags.h>
 #include <glog/logging.h>
 #include <map>
+#include <set>
 #include <unordered_set>
+#include <vector>
 
 #include "Index.h"
 
 DEFINE_uint64(entity_id, 0, "ID of the entity to print the call hierarchy of");
+DEFINE_uint64(reachable_from_entity_id, 0, "ID of the entity from which `--entity_id` must be reachable");
 
-static std::map<std::pair<mx::RawEntityId, mx::RawEntityId>, unsigned>
-    edge_weights;
+using Edge = std::pair<mx::RawEntityId, mx::RawEntityId>;
+static std::map<Edge, unsigned> edge_weights;
+static std::map<mx::RawEntityId, std::vector<mx::RawEntityId>> back_edges;
 
 static std::unordered_set<mx::RawEntityId> entities;
 
@@ -30,10 +34,12 @@ static void PrintCallGraph(mx::Stmt entity);
 void PrintCallGraphEdge(const mx::Decl &from_entity, mx::Decl to_entity) {
   auto from_id = from_entity.id();
   auto to_id = to_entity.id();
-  auto edge_key = std::make_pair<mx::RawEntityId, mx::RawEntityId>(
-      from_id, to_id);
+  auto edge_key = Edge(from_id, to_id);
 
   auto &edge_weight = edge_weights[edge_key];
+  if (!edge_weight && FLAGS_reachable_from_entity_id) {
+    back_edges[to_id].push_back(from_id);
+  }
   edge_weight += 1u;
   max_edge_weight = std::max(max_edge_weight, edge_weight);
 
@@ -129,8 +135,47 @@ extern "C" int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
+  std::set<Edge> reaching_edges;
+
+  // Find a restriction of the edges of the graph that are transitively
+  // reachable from `--reachable_from_entity_id`.
+  if (FLAGS_reachable_from_entity_id) {
+    auto maybe_reach_entity = index.entity(FLAGS_reachable_from_entity_id);
+    if (!std::holds_alternative<mx::Decl>(maybe_reach_entity)) {
+      std::cerr
+          << "Invalid reaching declaration id "
+          << FLAGS_reachable_from_entity_id << std::endl;
+      return EXIT_FAILURE;
+    }
+
+    mx::Decl reach_entity =
+        std::get<mx::Decl>(maybe_reach_entity).redeclarations()[0];
+
+    std::vector<mx::RawEntityId> next_reaching_edges;
+    std::vector<mx::RawEntityId> curr_reaching_edges;
+    next_reaching_edges.emplace_back(reach_entity.id());
+
+    while (!next_reaching_edges.empty()) {
+      curr_reaching_edges.swap(next_reaching_edges);
+      next_reaching_edges.clear();
+
+      for (mx::RawEntityId to_id : curr_reaching_edges) {
+        for (mx::RawEntityId from_id : back_edges[to_id]) {
+          Edge key(from_id, to_id);
+          if (reaching_edges.emplace(key).second) {
+            next_reaching_edges.push_back(from_id);
+          }
+        }
+      }
+    }
+  }
+
   auto mx = double(max_edge_weight);
   for (auto [edge_key, edge_weight] : edge_weights) {
+    if (FLAGS_reachable_from_entity_id && !reaching_edges.count(edge_key)) {
+      continue;
+    }
+
     auto w = static_cast<unsigned>((double(edge_weight) / mx) * 10) + 1u;
     std::cout
         << "  e" << edge_key.second << " -> e" << edge_key.first
