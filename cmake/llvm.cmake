a29c9461edef3f234c30ef148c86ce85293c30fe
kumarak <iit.akshay@gmail.com>
2022-07-19 21:36:41 -0400

    create debian package (#158)

--- cmake/llvm.cmake
+++ cmake/llvm.cmake
@@ -35,3 +35,47 @@ function(find_llvm target_name)
   )
 
 endfunction(find_llvm)
+
+# Install libraries for the target
+function(install_library target_name library_path)
+
+  add_custom_target(
+    ${target_name} ALL DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/lib/${target_name})
+
+  add_custom_command(
+    OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/lib/${target_name}
+    COMMAND ${CMAKE_COMMAND} -E copy ${library_path} "${CMAKE_CURRENT_BINARY_DIR}/lib/${target_name}"
+  )
+
+  install(FILES
+    ${CMAKE_CURRENT_BINARY_DIR}/lib/${file_name}
+      DESTINATION
+        ${CMAKE_INSTALL_LIBDIR}
+  )
+
+endfunction(install_library)
+
+# Find libLTO.so or libLTO.dylib. LLVM relies on these, and LLVM is used in
+# PASTA, which is used in mx-index and mx-import
+function(find_and_install_llvm_dependencies)
+
+  find_library(LIB_LTO LTO REQUIRED)
+  message(STATUS "Found LTO library: ${LIB_LTO}")
+  get_filename_component(lib_lto_name "${LIB_LTO}" NAME)
+  file(GLOB lib_lto_list ${LIB_LTO}*)
+
+  find_library(LIB_REMARKS Remarks REQUIRED)
+  message(STATUS "Found Remarks library: ${LIB_REMARKS}")
+  get_filename_component(lib_remarks_name "${LIB_REMARKS}" NAME)
+  file(GLOB lib_remarks_list ${LIB_REMARKS}*)
+
+  set(library_files ${lib_lto_list} ${lib_remarks_list})
+
+  foreach(file_i ${library_files})
+    get_filename_component(file_name "${file_i}" NAME)
+    if (NOT TARGET ${file_name})
+      install_library(${file_name} ${file_i})
+    endif()
+  endforeach(file_i)
+
+endfunction()
