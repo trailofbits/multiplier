9a84dc28f8c336b1c5f7bccd3442080878000ada
Andreas Kellas <andreas@wunused.com>
2022-07-21 18:23:39 -0400

    Check if vars are declared in loop for divergence

--- bin/Examples/FindDivergentCandidates.cpp
+++ bin/Examples/FindDivergentCandidates.cpp
@@ -14,6 +14,19 @@ DEFINE_uint64(file_id, 0, "ID of the file from which to check for potential cand
 DEFINE_uint64(fragment_id, 0, "ID of the fragment from which to check for potential candidates of divergent representations");
 DEFINE_bool(show_locations, false, "Show the locations of candidates?");
 
+static bool IsVarDeclInLoop(const mx::VarDecl var) {
+  auto parent_stmt = var.parent_statement();
+  while (parent_stmt) {
+    if (parent_stmt->kind() == mx::StmtKind::FOR_STMT || 
+        parent_stmt->kind() == mx::StmtKind::WHILE_STMT ||
+        parent_stmt->kind() == mx::StmtKind::DO_STMT) {
+      return true;
+    }
+    parent_stmt = parent_stmt->parent_statement();
+  }
+  return false;
+}
+
 static void FindDivergentCandidates(const mx::Fragment fragment) {
   /*
    * - int variable defined/declared outside of the for or while loop
@@ -23,6 +36,22 @@ static void FindDivergentCandidates(const mx::Fragment fragment) {
    * - variable is also used elsewhere, e.g. in a comparison operator, or 
    *   outside of the loop.
    */
+  for (mx::VarDecl var : mx::VarDecl::in(fragment)) {
+
+    std::cout
+        << fragment.id() << '\t'
+        << var.id() << '\t'
+        << var.name() 
+        << std::endl;
+      
+    auto parent_stmt = var.parent_statement();
+    while (parent_stmt) {
+      std::cout << mx::EnumeratorName(parent_stmt->kind()) << " -> ";
+      parent_stmt = parent_stmt->parent_statement();
+    }
+    std::cout << std::endl;
+    std::cout << (IsVarDeclInLoop(var) ? "true" : "false") << std::endl;
+  }
   return;
 }
 
@@ -38,7 +67,14 @@ extern "C" int main(int argc, char *argv[]) {
 
   mx::Index index = InitExample(FLAGS_show_locations);
 
-  if (FLAGS_file_id) {
+  if (FLAGS_fragment_id) {
+    auto fragment = index.fragment(FLAGS_fragment_id);
+    if (!fragment) {
+      std::cerr << "Invalid fragment id " << FLAGS_fragment_id << std::endl;
+      return EXIT_FAILURE;
+    }
+    FindDivergentCandidates(std::move(*fragment));
+  } else if (FLAGS_file_id) {
     auto file = index.file(FLAGS_file_id);
     if (!file) {
       std::cerr << "Invalid file id " << FLAGS_file_id << std::endl;
