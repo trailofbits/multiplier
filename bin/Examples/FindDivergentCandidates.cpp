86659870dde23c59d5cb53e17985a82212b9c0f9
Andreas Kellas <andreas@wunused.com>
2022-07-22 19:14:42 -0400

    Complete messy but working FindDivergentCandidates

--- bin/Examples/FindDivergentCandidates.cpp
+++ bin/Examples/FindDivergentCandidates.cpp
@@ -28,30 +28,101 @@ static bool IsVarDeclInLoop(const mx::VarDecl var) {
 }
 
 static void FindDivergentCandidates(const mx::Fragment fragment) {
-  /*
-   * - int variable defined/declared outside of the for or while loop
-   * - variable is incremented or decrement in the loop, or in the loop's 
-   *   increment section
-   * - variable is used to index into an array
-   * - variable is also used elsewhere, e.g. in a comparison operator, or 
-   *   outside of the loop.
-   */
-  for (mx::VarDecl var : mx::VarDecl::in(fragment)) {
-
-    std::cout
-        << fragment.id() << '\t'
-        << var.id() << '\t'
-        << var.name() 
-        << std::endl;
-      
-    auto parent_stmt = var.parent_statement();
-    while (parent_stmt) {
-      std::cout << mx::EnumeratorName(parent_stmt->kind()) << " -> ";
-      parent_stmt = parent_stmt->parent_statement();
+  /* 
+    Get all statements in a fragment, iterate through them.
+    For reach statement,
+    - Determine if statement is a loop. If not, continue.
+    - Determine whether a variable is incremented in fragment. If not, continue.
+    - For each incremented variable, 
+      + Determine if it is defined/declared outside of the loop statement. If 
+        not, continue.
+      + Determine if it is used to index into an array.
+      + Determine if it is used elsewhere outside of the loop.
+  */
+  for (mx::Stmt stmt : mx::Stmt::in(fragment)) {
+    /* Determine whether a statement is a loop. */
+    mx::StmtKind kind = stmt.kind();
+    if (kind != mx::StmtKind::FOR_STMT &&
+        kind != mx::StmtKind::WHILE_STMT &&
+        kind != mx::StmtKind::DO_STMT) {
+      continue;
     }
-    std::cout << std::endl;
-    std::cout << (IsVarDeclInLoop(var) ? "true" : "false") << std::endl;
-  }
+
+    /* Determine whether a variable is incremented in the loop. */
+    for (mx::Stmt child : stmt.children()) {
+      if (child.kind() != mx::StmtKind::UNARY_OPERATOR) { continue; }
+      if (auto unary_op = mx::UnaryOperator::from(child)) {
+        mx::UnaryOperatorKind operator_kind = unary_op->opcode();
+        switch (operator_kind) {
+          case mx::UnaryOperatorKind::POST_DEC:
+          case mx::UnaryOperatorKind::PRE_DEC:
+          case mx::UnaryOperatorKind::POST_INCREMENT:
+          case mx::UnaryOperatorKind::PRE_INCREMENT:
+            break;
+          default:
+            continue;
+            break;
+        }
+        /* Determine whether the variable is declared outside the loop. */
+        mx::Expr sub_expr = unary_op->sub_expression();
+        if (auto decl_ref = mx::DeclRefExpr::from(sub_expr)) {
+          mx::ValueDecl decl = decl_ref->declaration();
+          if (stmt.contains(decl.token())) {
+            continue;
+          }
+          
+          /* 
+            Determine whether the variable is used to index into an array, and
+            whether it is used elsewhere outside the loop.
+           */
+          bool mem_access = false;
+          bool used_out_of_loop = false;
+          mx::ArraySubscriptExpr *example_mem_access;
+          mx::Stmt *outside_loop_use;
+
+          for (const mx::Reference ref : decl.references()) {
+            mx::Stmt ref_stmt = ref.statement();
+            if (auto parent = ref_stmt.parent_statement()) { 
+              if (parent->kind() == mx::StmtKind::IMPLICIT_CAST_EXPR) {
+                // Might be None 
+                // TODO: Check
+                parent = parent->parent_statement();
+              }
+              if (auto array_access = mx::ArraySubscriptExpr::from(parent)) {
+                // TODO: Did I just throw in a use-after-free for good luck?
+                example_mem_access = &(*array_access);
+                mem_access = true;
+                continue;
+              }
+              if (!stmt.contains(*parent)) {
+                // TODO: Did I just throw in a use-after-free for good luck?
+                outside_loop_use = &(*parent);
+                used_out_of_loop = true;
+              }
+            }
+          }
+          if (mem_access && used_out_of_loop) {
+            // Found a candidate - let's print stuff about it.
+            std::cout << "-------------------" << std::endl;
+            std::cout << "candidate decl: " << decl.tokens().data() << std::endl;
+            std::cout 
+                  << "increment/decrement inside loop: "
+                  << unary_op->tokens().data() 
+                  << std::endl;
+            std::cout
+                  << "memory access: "
+                  << example_mem_access->tokens().data()
+                  << std::endl;
+            std::cout
+                  << "example use outside loop: "
+                  << outside_loop_use->tokens().data()
+                  << std::endl;
+          }
+        }
+      }
+
+    }
+  } 
   return;
 }
 
