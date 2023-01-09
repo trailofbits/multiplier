trailofbits/multiplier

commit d6275d51a7c1e6e953f13637bb9d49e0cecf7952

Store file_ids and fragment_ids instead of indexes to make diagnostic joins easier.

include/multiplier/Database.h
@@ -145,7 +145,16 @@ struct MangledNameRecord {
 
   static constexpr const char *kExitStatements[] = {
       R"(CREATE INDEX IF NOT EXISTS mangled_name_from_entity_id
-         ON mangled_name(data))"};
+         ON mangled_name(data))",
+
+      // Mangle name data columns use space-delimited data. Having a view that
+      // can give us access to just the normal names is nifty when we're trying
+      // to diagnose issues where we have logically the same entity mangled to
+      // two different names, e.g. `static inline` functions in headers.
+      R"(CREATE VIEW IF NOT EXISTS base_mangled_name AS
+         SELECT m.entity_id AS entity_id,
+                substr(m.data||' ', 0, instr(m.data||' ',' ')) AS data
+         FROM mangled_name AS m)"};
 
   static constexpr const char *kInsertStatement =
       R"(INSERT INTO mangled_name (entity_id, data)

lib/Database.cpp
@@ -80,7 +80,7 @@ class WriterThreadState {
 };
 
 RawEntityId WriterThreadState::GetOrCreateFileId(
-    RawEntityId proposed_index, const std::string &hash) {
+    RawEntityId proposed_id, const std::string &hash) {
 
   // Calculate which file table to index into.
   const size_t table = kHasher(hash) % kNumFileShards;
@@ -91,36 +91,37 @@ RawEntityId WriterThreadState::GetOrCreateFileId(
   if (!get) {
     auto table_name = "file_hash_" + std::to_string(table);
     get.emplace(db.Prepare(
-        "SELECT file_index FROM " + table_name + " WHERE hash = ?1")),
+        "SELECT file_id FROM " + table_name + " WHERE hash = ?1")),
 
     set.emplace(db.Prepare(
-        "INSERT INTO " + table_name + " (file_index, hash) "
+        "INSERT INTO " + table_name + " (file_id, hash) "
         "VALUES (?1, ?2) "
-        "ON CONFLICT DO UPDATE SET file_index = file_index "
-        "RETURNING file_index"));
+        "ON CONFLICT DO UPDATE SET file_id = file_id "
+        "RETURNING file_id"));
   }
 
-  RawEntityId index_out = kInvalidEntityId;
+  RawEntityId id_out = kInvalidEntityId;
 
   get->BindValues(hash);
   if (get->ExecuteStep()) {
-    get->Row().Columns(index_out);
+    get->Row().Columns(id_out);
   }
   get->Reset();
 
-  while (index_out == kInvalidEntityId) {
-    set->BindValues(proposed_index, hash);
+
+  while (id_out == kInvalidEntityId) {
+    set->BindValues(proposed_id, hash);
     if (set->ExecuteStep()) {
-      set->Row().Columns(index_out);
+      set->Row().Columns(id_out);
     }
     set->Reset();
   }
 
-  return index_out;
+  return id_out;
 }
 
 RawEntityId WriterThreadState::GetOrCreateFragmentId(
-    RawEntityId proposed_index, RawEntityId file_tok_id,
+    RawEntityId proposed_id, RawEntityId file_tok_id,
     const std::string &hash) {
 
   // Calculate which fragment table to index into.
@@ -132,32 +133,33 @@ RawEntityId WriterThreadState::GetOrCreateFragmentId(
   if (!get) {
     auto table_name = "fragment_hash_" + std::to_string(table);
     get.emplace(db.Prepare(
-        "SELECT fragment_index FROM " + table_name +
+        "SELECT fragment_id FROM " + table_name +
         " WHERE file_token_id = ?1 AND hash = ?2")),
 
     set.emplace(db.Prepare(
-        "INSERT INTO " + table_name + " (fragment_index, file_token_id, hash) "
+        "INSERT INTO " + table_name + " (fragment_id, file_token_id, hash) "
         "VALUES (?1, ?2, ?3) "
-        "ON CONFLICT DO UPDATE SET fragment_index = fragment_index "
-        "RETURNING fragment_index"));
+        "ON CONFLICT DO UPDATE SET fragment_id = fragment_id "
+        "RETURNING fragment_id"));
   }
 
-  RawEntityId index_out = kInvalidEntityId;
+  RawEntityId id_out = kInvalidEntityId;
+
   get->BindValues(file_tok_id, hash);
   if (get->ExecuteStep()) {
-    get->Row().Columns(index_out);
+    get->Row().Columns(id_out);
   }
   get->Reset();
 
-  while (index_out == kInvalidEntityId) {
-    set->BindValues(proposed_index, file_tok_id, hash);
+  while (id_out == kInvalidEntityId) {
+    set->BindValues(proposed_id, file_tok_id, hash);
     if (set->ExecuteStep()) {
-      set->Row().Columns(index_out);
+      set->Row().Columns(id_out);
     }
     set->Reset();
   }
 
-  return index_out;
+  return id_out;
 }
 
 class BulkInserterState {
@@ -528,16 +530,16 @@ SpecificEntityId<FileId> DatabaseWriter::GetOrCreateFileIdForHash(
     writer->available_file_index.reset();
   }
 
-  RawEntityId found_index = writer->GetOrCreateFileId(proposed_index, hash);
-  is_new = found_index == proposed_index;
+  RawEntityId proposed_id = EntityId(FileId(proposed_index)).Pack();
+  RawEntityId found_id = writer->GetOrCreateFileId(proposed_id, hash);
+  is_new = found_id == proposed_id;
   if (!is_new) {
     writer->available_file_index.emplace(proposed_index);
   }
 
-  assert(found_index != kInvalidEntityId);
-
-  FileId ret(found_index);
-  return ret;
+  VariantId vid = EntityId(found_id).Unpack();
+  assert(std::holds_alternative<FileId>(vid));
+  return std::get<FileId>(vid);
 }
 
 // Get, or create and return, a fragment ID for the specific fragment hash.
@@ -556,16 +558,17 @@ DatabaseWriter::GetOrCreateSmallFragmentIdForHash(
     writer->available_small_fragment_index.reset();
   }
 
-  RawEntityId found_index = writer->GetOrCreateFragmentId(
-      proposed_index, tok_id, hash);
-  is_new = found_index == proposed_index;
+  RawEntityId proposed_id = EntityId(FragmentId(proposed_index)).Pack();
+  RawEntityId found_id = writer->GetOrCreateFragmentId(
+      proposed_id, tok_id, hash);
+  is_new = found_id == proposed_id;
   if (!is_new) {
     writer->available_small_fragment_index.emplace(proposed_index);
   }
 
-  assert(found_index != kInvalidEntityId);
-
-  return FragmentId(found_index);
+  VariantId vid = EntityId(found_id).Unpack();
+  assert(std::holds_alternative<FragmentId>(vid));
+  return std::get<FragmentId>(vid);
 }
 
 // Get, or create and return, a fragment ID for the specific fragment hash.
@@ -584,16 +587,17 @@ DatabaseWriter::GetOrCreateLargeFragmentIdForHash(
     writer->available_big_fragment_index.reset();
   }
 
-  RawEntityId found_index = writer->GetOrCreateFragmentId(
-      proposed_index, tok_id, std::move(hash));
-  is_new = found_index == proposed_index;
+  RawEntityId proposed_id = EntityId(FragmentId(proposed_index)).Pack();
+  RawEntityId found_id = writer->GetOrCreateFragmentId(
+      proposed_id, tok_id, std::move(hash));
+  is_new = found_id == proposed_index;
   if (!is_new) {
     writer->available_big_fragment_index.emplace(proposed_index);
   }
 
-  assert(found_index != kInvalidEntityId);
-
-  return FragmentId(found_index);
+  VariantId vid = EntityId(found_id).Unpack();
+  assert(std::holds_alternative<FragmentId>(vid));
+  return std::get<FragmentId>(vid);
 }
 
 void DatabaseWriterImpl::InitRecords(void) {

commit 04267f33f134f34a0ff7d825f80e66c51bd58088

Fixes a bug related to large fragment ids. Bug was introduced in d6275d51a7c1e6e953f13637bb9d49e0cecf7952.

bin/Index/PendingFragment.h
@@ -61,12 +61,12 @@ class FileLocationOfFragment {
 // represents a single logical thing.
 class PendingFragment {
  public:
-  inline PendingFragment(mx::SpecificEntityId<mx::FragmentId> fragment_id_)
+  inline PendingFragment(mx::PackedFragmentId fragment_id_)
       : fragment_id(fragment_id_),
         fragment_index(fragment_id.Unpack().fragment_id) {}
 
   // Unique ID of the fragment containing the top-level declarations `decls`.
-  mx::SpecificEntityId<mx::FragmentId> fragment_id;
+  mx::PackedFragmentId fragment_id;
   mx::RawEntityId fragment_index;
 
   // Inclusive range of indices into the parsed tokens.

include/multiplier/Database.h
@@ -315,18 +315,18 @@ class DatabaseWriter final {
   void AsyncFlush(void);
 
   // Get, or create and return, a file ID for the specific file contents hash.
-  SpecificEntityId<FileId> GetOrCreateFileIdForHash(
+  PackedFileId GetOrCreateFileIdForHash(
       std::string hash, bool &is_new);
 
   // Get, or create and return, a fragment ID for the specific fragment hash.
-  SpecificEntityId<FragmentId> GetOrCreateSmallFragmentIdForHash(
+  PackedFragmentId GetOrCreateSmallFragmentIdForHash(
       RawEntityId tok_id, std::string hash, bool &is_new);
 
   // Get, or create and return, a fragment ID for the specific fragment hash.
-  SpecificEntityId<FragmentId> GetOrCreateLargeFragmentIdForHash(
+  PackedFragmentId GetOrCreateLargeFragmentIdForHash(
       RawEntityId tok_id, std::string hash, bool &is_new);
 
-  SpecificEntityId<FragmentId> GetOrCreateFragmentIdForHash(
+  PackedFragmentId GetOrCreateFragmentIdForHash(
       RawEntityId tok_id, std::string hash, size_t num_tokens, bool &is_new) {
 
     // "Big codes" have IDs in the range [1, mx::kMaxNumBigPendingFragments)`.

lib/Database.cpp
@@ -557,6 +557,7 @@ DatabaseWriter::GetOrCreateSmallFragmentIdForHash(
     proposed_index = writer->available_small_fragment_index.value();
     writer->available_small_fragment_index.reset();
   }
+  assert(proposed_index >= kMaxBigFragmentId);
 
   RawEntityId proposed_id = EntityId(FragmentId(proposed_index)).Pack();
   RawEntityId found_id = writer->GetOrCreateFragmentId(
@@ -568,7 +569,9 @@ DatabaseWriter::GetOrCreateSmallFragmentIdForHash(
 
   VariantId vid = EntityId(found_id).Unpack();
   assert(std::holds_alternative<FragmentId>(vid));
-  return std::get<FragmentId>(vid);
+  FragmentId fid = std::get<FragmentId>(vid);
+  assert(fid.fragment_id >= kMaxBigFragmentId);
+  return fid;
 }
 
 // Get, or create and return, a fragment ID for the specific fragment hash.
@@ -587,17 +590,21 @@ DatabaseWriter::GetOrCreateLargeFragmentIdForHash(
     writer->available_big_fragment_index.reset();
   }
 
+  assert(proposed_index < kMaxBigFragmentId);
+
   RawEntityId proposed_id = EntityId(FragmentId(proposed_index)).Pack();
   RawEntityId found_id = writer->GetOrCreateFragmentId(
       proposed_id, tok_id, std::move(hash));
-  is_new = found_id == proposed_index;
+  is_new = found_id == proposed_id;
   if (!is_new) {
     writer->available_big_fragment_index.emplace(proposed_index);
   }
 
   VariantId vid = EntityId(found_id).Unpack();
   assert(std::holds_alternative<FragmentId>(vid));
-  return std::get<FragmentId>(vid);
+  FragmentId fid = std::get<FragmentId>(vid);
+  assert(fid.fragment_id < kMaxBigFragmentId);
+  return fid;
 }
 
 void DatabaseWriterImpl::InitRecords(void) {