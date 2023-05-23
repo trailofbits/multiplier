5c2011b7da1e2d95c692c3d0108016a0c77a237c
Akshay K <iit.akshay@gmail.com>
2023-05-23 00:22:58 -0400

    fixup! remove debug logs

--- lib/SourceIR.cpp
+++ lib/SourceIR.cpp
@@ -168,8 +168,6 @@ SourceIRImpl::SourceIRImpl(std::shared_ptr<const FragmentImpl> frag_,
   mod = mlir::parseSourceFile<mlir::ModuleOp>(sm, &mctx);
   if (mod) {
     deserialized_ops = Deserialize(mod.get());
-  } else {
-    std::cerr << "Failed to lower: " << frag->fragment_id << "\n" << mlir << std::endl;
   }
 }
 
