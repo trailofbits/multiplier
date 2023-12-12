0e5d01df2e32b78d22ae747da23c28e51661b019
Peter Goodman <peter@trailofbits.com>
2023-12-12 18:46:21 -0500

    Modify linker requirements for macOS

--- cmake/linker.cmake
+++ cmake/linker.cmake
@@ -7,6 +7,9 @@
 #
 
 # MLIR requires that `lld` be used.
-set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld")
-set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=lld")
-set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=lld")
+
+if(PLATFORM_LINUX)
+  set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld")
+  set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=lld")
+  set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=lld")
+endif(PLATFORM_LINUX)
