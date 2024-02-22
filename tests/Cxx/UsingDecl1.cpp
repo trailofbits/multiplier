35d7bb8dd81f04ade6706d650d59e301d92f15ea
Akshay K <iit.akshay@gmail.com>
2024-02-22 11:41:02 -0500

    Update hashing and create specialization as floating fragments

--- tests/Cxx/UsingDecl1.cpp
+++ tests/Cxx/UsingDecl1.cpp
@@ -2,10 +2,14 @@
 namespace A {
     struct B { };
 <<<<<<< HEAD
+<<<<<<< HEAD
     void operator+(B, B);
 =======
     void operator+(B,B);
 >>>>>>> 5d49e713d (Intial changes to fix root fragment for function template)
+=======
+    void operator+(B, B);
+>>>>>>> f715bd801 (Update hashing and create specialization as floating fragments)
 }
 
 using A::operator+;
